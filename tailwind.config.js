5be9fbe7741fbcd94ba860e062483e62c49ab89b
easysajusaju-dev <easysaju.saju@gmail.com>
2025-11-20 22:04:02 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["system-ui", "Apple SD Gothic Neo", "Noto Sans KR", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
